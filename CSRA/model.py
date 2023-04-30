be1380dba63520f511ffaa30c55e1bebff20b60d
leedujin <96468353+leedujin@users.noreply.github.com>
2023-04-30 18:32:01 +0900

    Add files via upload

---
+++ CSRA/model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torchvision.models as models
+
+class BaseModel(nn.Module):
+    def __init__(self, num_classes=10):
+        super(BaseModel, self).__init__()
+        self.backbone = models.efficientnet_b3(pretrained=True)
+        self.classifier = nn.Linear(1000, num_classes)
+        
+        ######
+        
+    def forward(self, x):
+        x = self.backbone(x)
+        
+        ######
+        
+        x = F.sigmoid(self.classifier(x))
+        return x
